887cd13009c823634531e4d2f6362568fb905ff2
Jim MacKenzie <protojazz@gmail.com>
2025-09-19 10:06:58 -0500

    migrate

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
+
 echo "Running database migrations..."
-bin/birds_against_mortality eval "BirdsAgainstMortality.Release.migrate()"
+bin/birds_against_mortality eval "BirdsAgainstMortality.Release.migrate"
 echo "Migrations completed successfully!"
 
 echo "Running database seeds..."
-bin/birds_against_mortality eval "BirdsAgainstMortality.Release.seed()"
+bin/birds_against_mortality eval "BirdsAgainstMortality.Release.seed"
 echo "Seeds completed successfully!"
